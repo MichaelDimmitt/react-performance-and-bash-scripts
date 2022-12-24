e44e73bbe888130b8f72d1c1a8dd0d0b899e3896
michael <michaelgdimmitt@gmail.com>
2022-12-23 23:36:22 -0500

    fix revert file was not working correctly

--- bash_scripts/generate-n-files.sh
+++ bash_scripts/generate-n-files.sh
@@ -1,45 +1,39 @@
-# variables
-# whereami="$PWD/$0"
-# folderlocation=$(dirname $whereami)
-# echo I am here: $folderlocation
+lib=$(dirname $PWD/$0)
+base=$lib/../project-one/src/
 
-allinonefolderlocation=$(dirname $PWD/$0)
-base=$allinonefolderlocation/../project-one/src/
-
-echo $base
 suffix=App.tsx;
 target=$base$suffix
 
 
-cp backup.tsx $target
+cp $lib/backup.tsx $target
 rm -rf temp1.txt;
 rm -rf temp2.txt;
 
 for ((i=1;i<=100;i++)); 
 do 
    # your-unix-command-here
-   echo "import Hello$i from './Hello$i'" >> temp1.txt;
+   echo "import Hello$i from './Hello$i'" >> $lib/temp1.txt;
 done
 
 for ((i=1;i<=100;i++)); 
 do 
    # your-unix-command-here
-   echo "<Hello$i/>" >> temp2.txt
+   echo "<Hello$i/>" >> $lib/temp2.txt
 done
 
 line2=21
-tmp="./sed.1.$$"
-sed -e "${line2}r temp2.txt" $target > $tmp
+tmp="$lib/sed.1.$$"
+sed -e "${line2}r $lib/temp2.txt" $target > $tmp
 cp $tmp $target
 
 line=3
-tmp="./sed.$$"
-sed -e "${line}r temp1.txt" $target > $tmp
+tmp="$lib/sed.$$"
+sed -e "${line}r $lib/temp1.txt" $target > $tmp
 cp $tmp $target
 
 
 for ((i=1;i<=100;i++)); 
 do 
    # your-unix-command-here
-   cp backup.tsx ${base}Hello${i}\.tsx
+   cp $lib/backup.tsx ${base}Hello${i}\.tsx
 done
--- bash_scripts/revert.sh
+++ bash_scripts/revert.sh
@@ -1,12 +1,13 @@
+lib=$(dirname $PWD/$0)
+base=$lib/../project-one/src/
 
-allinonefolderlocation=$(dirname $PWD/$0)
-base=$allinonefolderlocation/../project-one/src/
 
+rm $lib/temp1.txt $lib/temp2.txt
+rm $lib/sed.*
+cp $lib/backup.tsx ${base}App.tsx;
 
-rm temp1.txt temp2.txt
-cp backup.tsx ${base}App.tsx;
 for ((i=1;i<=100;i++)); 
 do 
    # your-unix-command-here
-   rm ${base}Hello$i\.tsx
+   rm ${base}Hello$i\.tsx &>/dev/null
 done
